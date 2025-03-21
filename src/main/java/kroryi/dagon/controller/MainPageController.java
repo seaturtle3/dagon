1d07b66a7dce5f4afd199f40c9a11413f11289c0
seaturtle3 <fttf136@gmail.com>
2025-03-21 16:50:39 +0900

    Initial commit

---
+++ src/main/java/kroryi/dagon/controller/MainPageController.java
@@ -0,0 +1,14 @@
+package kroryi.dagon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "mainPage";
+    }
+}
---
+++ src/test/java/kroryi/dagon/DagonApplicationTests.java
@@ -0,0 +1,15 @@
+package kroryi.dagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagonApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+
+}
