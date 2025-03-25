f6cdf0b9048553efcdf6988100703d532b14f4e2
seaturtle3 <fttf136@gmail.com>
2025-03-25 09:39:04 +0900

    test

--- src/main/java/kroryi/dagon/controller/LoginPageController.java
+++ src/main/java/kroryi/dagon/controller/LoginPageController.java
@@ -1,4 +1,13 @@
 package kroryi.dagon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class LoginPageController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
