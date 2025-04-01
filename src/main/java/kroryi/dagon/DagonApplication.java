11884ff3b97c932dbd8ab8b46d8654d9114bfd0d
seaturtle3 <fttf136@gmail.com>
2025-04-01 17:13:12 +0900

    main update

--- src/main/java/kroryi/dagon/DagonApplication.java
+++ src/main/java/kroryi/dagon/DagonApplication.java
@@ -2,12 +2,11 @@ package kroryi.dagon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class DagonApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DagonApplication.class, args);
     }
-
 }
