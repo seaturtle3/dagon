d451de29adeb677500afd6c9cd84a5fad4735fda
kangjinseok <easyevergreen@naver.com>
2025-04-08 10:10:12 +0900

    fd, fr entity update

--- src/main/java/kroryi/dagon/entity/FishingReport.java
+++ src/main/java/kroryi/dagon/entity/FishingReport.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -23,13 +24,13 @@ public class FishingReport {
 
     @ColumnDefault("CURRENT_TIMESTAMP")
     @Column(name = "created_at", nullable = false)
-    private Instant createdAt;
+    private LocalDateTime createdAt;
 
     @Column(name = "fishing_at", nullable = false)
-    private Instant fishingAt;
+    private LocalDateTime fishingAt;
 
     @Column(name = "modify_at")
-    private Instant modifyAt;
+    private LocalDateTime modifyAt;
 
     @Column(name = "fr_title", nullable = false)
     private String frTitle;
