1d2003057cfd29dcf54cda4057ba1c91a2fa816c
JJang_ez <dmswjd9866@gmail.com>
2025-04-10 15:53:00 +0900

    TideApiClient 연결중 key오류

--- src/main/java/kroryi/dagon/DTO/TideInfoDTO.java
+++ src/main/java/kroryi/dagon/DTO/TideInfoDTO.java
@@ -1,4 +1,16 @@
 package kroryi.dagon.DTO;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class TideInfoDTO {
+    private Result result;
+
+    @Data
+    public static class Result{
+        private List<TideItemDTO> data;
+    }
+
 }
