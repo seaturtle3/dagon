4988dc90e45cecf3593d67b89f4847d5c4cbb8e8
JJang_ez <dmswjd9866@gmail.com>
2025-04-11 11:22:11 +0900

    TideStation DB

---
+++ src/main/java/kroryi/dagon/entity/TideStation.java
@@ -0,0 +1,25 @@
+package kroryi.dagon.entity;
+
+import jakarta.persistence.*;
+import kroryi.dagon.enums.ProdRegion;
+
+@Entity
+@Table(name = "tide_station")
+public class TideStation {
+    @Id
+    @Column(name = "station_code")
+    private String stationCode;
+
+    @Column(name = "station_name")
+    private String stationName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "region")
+    private ProdRegion region;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+}
---
+++ src/main/java/kroryi/dagon/repository/TideStationRepository.java
@@ -0,0 +1,7 @@
+package kroryi.dagon.repository;
+
+import kroryi.dagon.entity.TideStation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TideStationRepository extends JpaRepository<TideStation, Integer> {
+}
