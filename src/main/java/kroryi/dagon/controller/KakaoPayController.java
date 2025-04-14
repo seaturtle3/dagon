09b8fda3d8013acbe85d12a9d5f95a43f12454cb
kangjinseok <easyevergreen@naver.com>
2025-04-14 09:20:12 +0900

    병합

--- src/main/java/kroryi/dagon/controller/KakaoPayController.java
+++ src/main/java/kroryi/dagon/controller/KakaoPayController.java
@@ -1,41 +1,41 @@
-package kroryi.dagon.controller;
-
-import kroryi.dagon.service.KakaoPayService;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-@RequiredArgsConstructor
-@Log4j2
-public class KakaoPayController {
-
-    @Setter(onMethod_ = @Autowired)
-    private KakaoPayService kakaoPay;
-
-    @GetMapping("/kakaoPay")
-    public String kakaoPayGet() {
-        log.info("1111111111111111111111");
-
-        return "kakaopay";
-    }
-
-    @PostMapping("/kakaoPay")
-    public String kakaoPay() {
-        log.info("kakaoPay post.....................");
-
-        return "redirect:" + kakaoPay.kakaoPayReady();
-    }
-
-    @GetMapping("/kakaoPaySuccess")
-    public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
-        log.info("kakaoPay Success get................");
-        log.info("kakaoPaySuccess pg_token : {}", pg_token);
-    }
-}
+//package kroryi.dagon.controller;
+//
+//import kroryi.dagon.service.KakaoPayService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.Setter;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//@RequiredArgsConstructor
+//@Log4j2
+//public class KakaoPayController {
+//
+//    @Setter(onMethod_ = @Autowired)
+//    private KakaoPayService kakaoPay;
+//
+//    @GetMapping("/kakaoPay")
+//    public String kakaoPayGet() {
+//        log.info("1111111111111111111111");
+//
+//        return "kakaopay";
+//    }
+//
+//    @PostMapping("/kakaoPay")
+//    public String kakaoPay() {
+//        log.info("kakaoPay post.....................");
+//
+//        return "redirect:" + kakaoPay.kakaoPayReady();
+//    }
+//
+//    @GetMapping("/kakaoPaySuccess")
+//    public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+//        log.info("kakaoPay Success get................");
+//        log.info("kakaoPaySuccess pg_token : {}", pg_token);
+//    }
+//}
--- src/main/java/kroryi/dagon/service/KakaoPayService.java
+++ src/main/java/kroryi/dagon/service/KakaoPayService.java
@@ -1,64 +1,64 @@
-package kroryi.dagon.service;
-
-
-import kroryi.dagon.DTO.KakaoPayDTO;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-
-@Service
-@RequiredArgsConstructor
-@Log4j2
-public class KakaoPayService {
-
-    private static final String HOST = "https://kapi.kakao.com";
-
-    @Value("${kakao.admin}")
-    private String kakaoAdminKey;
-
-    private KakaoPayDTO kakaoPayDTO;
-
-    private final WebClient webClient = WebClient.builder()
-            .baseUrl(HOST)
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-            .build();
-
-    public String kakaoPayReady() {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("cid", "TC0ONETIME");
-        params.add("partner_order_id", "1001");
-        params.add("partner_user_id", "goguma");
-        params.add("item_name", "비둘기");
-        params.add("quantity", "1");
-        params.add("total_amount", "20000");
-        params.add("tax_free_amount", "100");
-        params.add("approval_url", "http://localhost:8080/");
-        params.add("cancel_url", "http://localhost:8080/kakaoPayCancle");
-        params.add("fail_url", "http://localhost:8080/kakaoPayFail");
-
-        try {
-            kakaoPayDTO = webClient.post()
-                    .uri("/v1/payment/ready")
-                    .header(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoAdminKey)
-                    .body(BodyInserters.fromFormData(params))
-                    .retrieve()
-                    .bodyToMono(KakaoPayDTO.class)
-                    .block();
-
-            log.info("카카오페이 요청 성공: {}", kakaoPayDTO);
-            return kakaoPayDTO != null ? kakaoPayDTO.getNext_redirect_pc_url() : "/pay";
-
-        } catch (Exception e) {
-            log.error("카카오페이 요청 실패", e);
-            return "/pay";
-        }
-    }
-}
+//package kroryi.dagon.service;
+//
+//
+//import kroryi.dagon.DTO.KakaoPayDTO;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.stereotype.Service;
+//import org.springframework.util.LinkedMultiValueMap;
+//import org.springframework.util.MultiValueMap;
+//import org.springframework.web.reactive.function.BodyInserters;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//@Service
+//@RequiredArgsConstructor
+//@Log4j2
+//public class KakaoPayService {
+//
+//    private static final String HOST = "https://kapi.kakao.com";
+//
+//    @Value("${kakao.admin}")
+//    private String kakaoAdminKey;
+//
+//    private KakaoPayDTO kakaoPayDTO;
+//
+//    private final WebClient webClient = WebClient.builder()
+//            .baseUrl(HOST)
+//            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+//            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+//            .build();
+//
+//    public String kakaoPayReady() {
+//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//        params.add("cid", "TC0ONETIME");
+//        params.add("partner_order_id", "1001");
+//        params.add("partner_user_id", "goguma");
+//        params.add("item_name", "비둘기");
+//        params.add("quantity", "1");
+//        params.add("total_amount", "20000");
+//        params.add("tax_free_amount", "100");
+//        params.add("approval_url", "http://localhost:8080/");
+//        params.add("cancel_url", "http://localhost:8080/kakaoPayCancle");
+//        params.add("fail_url", "http://localhost:8080/kakaoPayFail");
+//
+//        try {
+//            kakaoPayDTO = webClient.post()
+//                    .uri("/v1/payment/ready")
+//                    .header(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoAdminKey)
+//                    .body(BodyInserters.fromFormData(params))
+//                    .retrieve()
+//                    .bodyToMono(KakaoPayDTO.class)
+//                    .block();
+//
+//            log.info("카카오페이 요청 성공: {}", kakaoPayDTO);
+//            return kakaoPayDTO != null ? kakaoPayDTO.getNext_redirect_pc_url() : "/pay";
+//
+//        } catch (Exception e) {
+//            log.error("카카오페이 요청 실패", e);
+//            return "/pay";
+//        }
+//    }
+//}
