d77b8eec8e1742b45e5c831be3d13141d3e96bd6
JJang_ez <dmswjd9866@gmail.com>
2025-04-14 15:06:51 +0900

    multtae 1/3 완료

--- src/main/java/kroryi/dagon/controller/TideController.java
+++ src/main/java/kroryi/dagon/controller/TideController.java
@@ -2,6 +2,7 @@ package kroryi.dagon.controller;
 
 import kroryi.dagon.DTO.TideItemDTO;
 import kroryi.dagon.component.TideApiClient;
+import kroryi.dagon.entity.TideStation;
 import kroryi.dagon.enums.ProdRegion;
 import kroryi.dagon.repository.TideStationRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,21 +35,29 @@ public class TideController {
                                   @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                                   Model model) {
         model.addAttribute("regions", ProdRegion.values());
+        // 기본값 설정
+        if (region == null) region = "인천";
+        if (stationCode == null) stationCode = "DT_0001";
+        if (date == null) date = LocalDate.now();
 
-        if (region != null) {
-            ProdRegion prodRegion = ProdRegion.fromKorean(region);
-            model.addAttribute("stations", tideStationRepository.findByRegion(prodRegion));
-        }
+        // 물때 정보
+        String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        List<TideItemDTO> tideItems = tideApiClient.getTideItems(stationCode, formattedDate);
+        model.addAttribute("tideItems", tideItems);
 
-      if(stationCode != null && date != null) {
-          String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-          List<TideItemDTO> tideItems = tideApiClient.getTideItems(stationCode, formattedDate);
-          model.addAttribute("tideItems", tideItems);
-          model.addAttribute("stationCode", stationCode);
-          model.addAttribute("date",date);
-      }
+        // 지역 그룹핑 (Map<ProdRegion, List<TideStation>>)
+        Map<ProdRegion, List<TideStation>> groupedStations = Arrays.stream(ProdRegion.values())
+                .collect(Collectors.toMap(
+                        r -> r,
+                        r -> tideStationRepository.findByRegion(r)
+                ));
 
-      return "menu/multtae";
+        model.addAttribute("groupedStations", groupedStations);
+        model.addAttribute("region", region);
+        model.addAttribute("stationCode", stationCode);
+        model.addAttribute("date", date);
+
+        return "menu/multtae";
     }
 
 
