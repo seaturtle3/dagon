a13bbb80edfe160c2a119f72ff599f182e9a8371
seaturtle3 <fttf136@gmail.com>
2025-04-16 17:21:47 +0900

    son > temp

--- src/main/java/kroryi/dagon/controller/HomeController.java
+++ src/main/java/kroryi/dagon/controller/HomeController.java
@@ -3,6 +3,7 @@ package kroryi.dagon.controller;
 import kroryi.dagon.entity.ProductFishSpecies;
 import kroryi.dagon.enums.ProdRegion;
 import kroryi.dagon.repository.FishSpeciesRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
