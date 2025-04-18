2e5a8ca65228708f0a75c957980acb918dcfb6b3
JJang_ez <dmswjd9866@gmail.com>
2025-04-18 09:52:48 +0900

    NoticeRequestDTO, NoticeResponseDTO 생성

---
+++ src/main/java/kroryi/dagon/DTO/notice/NoticeResponseDTO.java
@@ -0,0 +1,20 @@
+package kroryi.dagon.DTO.notice;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NoticeResponseDTO {
+    private Long noticeId;
+    private String title;
+    private String content;
+    private String thumbnailUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifyAt;
+    private Integer views;
+    private Boolean isTop;
+    private String adminName;
+}
