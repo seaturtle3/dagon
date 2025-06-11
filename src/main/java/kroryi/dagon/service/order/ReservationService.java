bd51a39c86e9869e589398960a5417b76cc083d0
kangjinseok <easyevergreen@naver.com>
2025-06-11 16:49:00 +0900

    이미지 추가2

--- src/main/java/kroryi/dagon/service/order/ReservationService.java
+++ src/main/java/kroryi/dagon/service/order/ReservationService.java
@@ -2,6 +2,7 @@ package kroryi.dagon.service.order;
 
 import kroryi.dagon.DTO.ReservationDTO;
 import kroryi.dagon.enums.ReservationStatus;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class ReservationService {
 
     private static final List<ReservationDTO> reservations = new ArrayList<>();
