79f26e23dee509c821403fe98533a2ec0dc504ca
seaturtle3 <fttf136@gmail.com>
2025-06-17 15:22:13 +0900

    ApiFishingReportController 조황정보 ID 조회 수정

--- src/main/java/kroryi/dagon/controller/partner/community/ApiFishingReportController.java
+++ src/main/java/kroryi/dagon/controller/partner/community/ApiFishingReportController.java
@@ -78,7 +78,7 @@ public class ApiFishingReportController {
     }
 
     @Operation(summary = "조황정보 ID 조회")
-    @GetMapping("/get/{id}")
+    @GetMapping("/{id}")
     public ApiFishingReportDTO getFishingReport(@PathVariable Long id) {
         return apiFishingReportService.getFishingReportById(id);
     }
