2573524e0839d8ac42255d56ebcd71c1768edb8c
plusmail <plus4957@gmail.com>
2025-06-24 21:06:38 +0900

    이미지 디비에 넣기

--- src/main/java/kroryi/dagon/DTO/board/FishingCenter/ApiFishingReportImageDTO.java
+++ src/main/java/kroryi/dagon/DTO/board/FishingCenter/ApiFishingReportImageDTO.java
@@ -3,16 +3,24 @@ package kroryi.dagon.DTO.board.FishingCenter;
 import kroryi.dagon.entity.fishingCenter.FishingReportImage;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Base64;
 
 @Data
 @NoArgsConstructor
 public class ApiFishingReportImageDTO {
     private String imageUrl;
     private boolean isThumbnail;
+    private String imageData; // Base64 인코딩된 이미지 데이터
 
     public ApiFishingReportImageDTO(FishingReportImage image) {
         this.imageUrl = image.getImageUrl();
         this.isThumbnail = image.isThumbnail();
+        // imageData가 null이 아니면 Base64 인코딩
+        if (image.getImageData() != null) {
+            this.imageData = Base64.getEncoder().encodeToString(image.getImageData());
+        } else {
+            this.imageData = null;
+        }
     }
 }
 
