1d843074aac093e1a7d96adf09f596001812d7b4
plusmail <plus4957@gmail.com>
2025-07-10 00:15:14 +0900

    ㅇㅇㅇㅇㅇㅇ

--- src/main/java/kroryi/dagon/repository/board/FAQRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package kroryi.dagon.repository.board;
-
-import kroryi.dagon.entity.FAQ;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface FAQRepositoryCustom {
-    Page<FAQ> searchDynamic(String keyword, Long categoryId, Boolean isActive, Pageable pageable);
-} 
