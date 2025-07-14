b078ecb73206a69ab0ffc99db0b546836cb21f4d
plusmail <plus4957@gmail.com>
2025-07-14 19:13:47 +0900

    https 적용

--- src/main/java/kroryi/dagon/repository/product/ProductRepository.java
+++ src/main/java/kroryi/dagon/repository/product/ProductRepository.java
@@ -53,12 +53,12 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
     //  -------------- 프론트 api 상단 필터 > 바다/민물 상품 가져오기 ----------------
     @Query("""
             SELECT DISTINCT p FROM Product p
-            LEFT JOIN p.fishSpeciesMappings m
-            LEFT JOIN m.fs fs
+            LEFT JOIN FETCH p.fishSpeciesMappings m
+            LEFT JOIN FETCH m.fs fs
             WHERE p.mainType = kroryi.dagon.enums.MainType.SEA
             AND (:region IS NULL OR p.prodRegion = :region)
             AND (:subType IS NULL OR p.subType = :subType)
-            AND (COALESCE(:species, NULL) IS NULL OR fs.fsName IN (:species))
+            AND ((:species) IS NULL OR fs.fsName IN (:species))
             """)
     Page<Product> findSeaProductsByFilters(
             @Param("region") ProdRegion region,
@@ -69,12 +69,12 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
 
     @Query("""
             SELECT DISTINCT p FROM Product p
-            LEFT JOIN p.fishSpeciesMappings m
-            LEFT JOIN m.fs fs
+            LEFT JOIN FETCH p.fishSpeciesMappings m
+            LEFT JOIN FETCH m.fs fs
             WHERE p.mainType = kroryi.dagon.enums.MainType.FRESHWATER
             AND (:region IS NULL OR p.prodRegion = :region)
             AND (:subType IS NULL OR p.subType = :subType)
-            AND (COALESCE(:species, NULL) IS NULL OR fs.fsName IN (:species))
+            AND ((:species) IS NULL OR fs.fsName IN (:species))
             """)
     Page<Product> findFreshwaterProductsByFilters(
             @Param("region") ProdRegion region,
