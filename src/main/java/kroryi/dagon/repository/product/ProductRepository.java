95adf8e1b99f18a6191f8cf5469fe5960ee51aad
plusmail <plus4957@gmail.com>
2025-07-14 19:32:01 +0900

    https 적용

--- src/main/java/kroryi/dagon/repository/product/ProductRepository.java
+++ src/main/java/kroryi/dagon/repository/product/ProductRepository.java
@@ -56,8 +56,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
             WHERE p.mainType = kroryi.dagon.enums.MainType.SEA
             AND (:region IS NULL OR p.prodRegion = :region)
             AND (:subType IS NULL OR p.subType = :subType)
-            AND ((:species) IS NULL OR :species IS EMPTY OR 
-                 EXISTS (SELECT 1 FROM p.fishSpeciesMappings m JOIN m.fs fs WHERE fs.fsName IN (:species)))
             """)
     Page<Product> findSeaProductsByFilters(
             @Param("region") ProdRegion region,
@@ -71,8 +69,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
             WHERE p.mainType = kroryi.dagon.enums.MainType.FRESHWATER
             AND (:region IS NULL OR p.prodRegion = :region)
             AND (:subType IS NULL OR p.subType = :subType)
-            AND ((:species) IS NULL OR :species IS EMPTY OR 
-                 EXISTS (SELECT 1 FROM p.fishSpeciesMappings m JOIN m.fs fs WHERE fs.fsName IN (:species)))
             """)
     Page<Product> findFreshwaterProductsByFilters(
             @Param("region") ProdRegion region,
